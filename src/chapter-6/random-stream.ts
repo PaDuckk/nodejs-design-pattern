b67b2de13a3814088f0eab6d2189099c6d507299
PaDuckk <kang10019@gmail.com>
2021-08-05 00:21:31 +0900

    fi

--- src/chapter-6/random-stream.ts
+++ src/chapter-6/random-stream.ts
@@ -1,4 +1,23 @@
-import { Readable } from "stream";
+import { Readable, ReadableOptions } from "stream";
 import Chance from "chance";
 
 const chance = new Chance();
+
+export class RandomStream extends Readable {
+  emittedBytes: number;
+
+  constructor(options: ReadableOptions) {
+    super(options);
+
+    this.emittedBytes = 0;
+  }
+
+  _read(size: number) {
+    const chunk = chance.string({ length: size });
+    this.push(chunk, "utf-8");
+    this.emittedBytes += chunk.length;
+    if (chance.bool({ likelihood: 5 })) {
+      this.push(null);
+    }
+  }
+}
